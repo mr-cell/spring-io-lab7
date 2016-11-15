4edc0abca4766776e62ac825013d3bffd02ec3a6
marcel.schally <marcel.schally@gmail.com>
2016-11-15 14:15:23 +0100

    added client request for reservation names

--- reservation-client/src/main/java/com/example/ReservationClientApplication.java
+++ reservation-client/src/main/java/com/example/ReservationClientApplication.java
@@ -1,13 +1,28 @@
 package com.example;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
@@ -31,4 +46,40 @@ class ReservationsExtras {
 			});
 		};
 	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
+
+@Slf4j
+@RestController
+@RequestMapping("/reservations")
+class ReservationClientController {
+	
+	private RestTemplate restTemplate;
+	
+	ReservationClientController(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+	
+	@RequestMapping(path = "/names", method = RequestMethod.GET)
+	public List<String> getNames() {
+		ParameterizedTypeReference<Resources<ReservationPayload>> resultType = 
+				new ParameterizedTypeReference<Resources<ReservationPayload>>() { };
+		ResponseEntity<Resources<ReservationPayload>> reservations =
+				restTemplate.exchange("http://reservationservice/reservations", HttpMethod.GET, null, resultType);
+		return reservations.getBody().getContent().stream()
+				.map(ReservationPayload::getName)
+				.collect(Collectors.toList());		
+	}
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class ReservationPayload {
+	String name;
 }
